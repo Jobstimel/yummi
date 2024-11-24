51654bda8a5b2b999b87ec0cbc5be32c90a2326f
Jobstimel <chris.jobst@hotmail.com>
2024-11-24 08:47:45 +0100

    Start inspiration page design

--- lib/pages/inspiration_page.dart
+++ lib/pages/inspiration_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yummi/constants.dart';
 
 class InspirationPage extends StatefulWidget {
   const InspirationPage({super.key});
@@ -18,8 +19,44 @@ class _InspirationPageState extends State<InspirationPage> {
           "Inspiration",
         ),
       ),
-      body: Text(
-        "InspirationPage",
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  "assets/food_generic.jpeg",
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "Nudeln mit Tomatensoße",
+              style: TextStyle(
+                color: Constants.PRIMARY_COLOR,
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
+            Text(
+              "vor 15 Tagen",
+              style: TextStyle(
+                color: Constants.SUBTITLE_COLOR,
+                fontSize: 13,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
